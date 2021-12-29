a476822698282202404be45a2c33077f1215a5cd
mokuo <tennis10988.yk@gmail.com>
2021-12-29 18:24:12 +0900

    確実に Browser をクローズする

--- src/infrastructure/TennisCourtFrames/__tests__/ItabashiReservationSystemRepository.test.ts
+++ src/infrastructure/TennisCourtFrames/__tests__/ItabashiReservationSystemRepository.test.ts
@@ -1,12 +1,20 @@
-import { chromium } from "playwright";
+import { chromium, Browser } from "playwright";
 import ItabashiReservationSystemRepository from "../ItabashiReservationSystemRepository";
 
 // fix: `thrown: "Exceeded timeout of 5000 ms for a test.`
 jest.setTimeout(30000);
 
 describe("ItabashiReservationSystemRepository", () => {
+  // ref: https://playwright.tech/blog/using-jest-with-playwright
+  let browser: Browser;
+  beforeAll(async () => {
+    browser = await chromium.launch();
+  });
+  afterAll(async () => {
+    await browser.close();
+  });
+
   it("#getTennisCourts", async () => {
-    const browser = await chromium.launch();
     const repo = new ItabashiReservationSystemRepository(browser);
     const tennisCourts = await repo.all();
     await browser.close();
