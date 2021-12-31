7921c5925cb583a46e0e53a15d7684d82a18f3e3
mokuo <tennis10988.yk@gmail.com>
2021-12-31 18:13:45 +0900

    fix eslint

--- src/infrastructure/TennisCourtFrames/ItabashiReservationSystemRepository.ts
+++ src/infrastructure/TennisCourtFrames/ItabashiReservationSystemRepository.ts
@@ -3,7 +3,6 @@ import ItabashiTennisCourtTable from "./ItabashiTennisCourtTable";
 import IReservationSystemRepository from "@src/domain/models/TennisCourtFrames/IReservationSystemRepository";
 import TennisCourtFrame from "@src/domain/models/TennisCourtFrames/TennisCourtFrame";
 import parseHtmlTable from "@src/lib/parseHtmlTable";
-import sleep from "@src/lib/sleep";
 
 const URL = "https://www.itabashi-shisetsu-yoyaku.jp/eshisetsu/menu/Login.cgi";
 const TABLE_SELECTOR = "table [summary='選択した施設・時間帯の空き状況を確認するための表。']";
