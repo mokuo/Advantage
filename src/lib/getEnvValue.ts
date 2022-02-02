7e3b87bc644f3136a52192a59b66b1e3c36e7adf
mokuo <tennis10988.yk@gmail.com>
2022-02-03 08:58:22 +0900

    fix test

--- src/lib/getEnvValue.ts
+++ src/lib/getEnvValue.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 class UndefinedEnvironmentVariableError extends Error {}
 
-const getEnvValue = (envName = "CHANNEL_ACCESS_TOKEN"): string => {
+const getEnvValue = (envName: string): string => {
   const value = process.env[envName];
   if (value === undefined) {
     throw new UndefinedEnvironmentVariableError(`${envName} is undefined.`);
