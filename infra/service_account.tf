b1818dc2ebfca996fc7611706efd48c9dce8d676
mokuo <tennis10988.yk@gmail.com>
2022-04-06 09:00:39 +0900

    infra: Cloud Scheduler で定期実行する

--- infra/service_account.tf
+++ infra/service_account.tf
@@ -11,3 +11,16 @@ resource "google_project_iam_member" "advantage_cloud_run_firestore" {
   role = "roles/datastore.user"
   member = "serviceAccount:${google_service_account.advantage_cloud_run.email}"
 }
+
+# ref: https://cloud.google.com/run/docs/triggering/using-scheduler?hl=ja
+resource "google_service_account" "advantage_cloud_scheduler" {
+  project = "mokuo-advantage"
+  account_id   = "advantage-cloud-scheduler"
+  display_name = "Advantage Cloud Scheduler Service Account"
+}
+resource "google_project_iam_member" "advantage_cloud_scheduler_cloud_run" {
+  project = "mokuo-advantage"
+  role = "roles/run.invoker" # https://cloud.google.com/iam/docs/understanding-roles?hl=ja#cloud-run-roles
+  member = "serviceAccount:${google_service_account.advantage_cloud_scheduler.email}"
+}
+
